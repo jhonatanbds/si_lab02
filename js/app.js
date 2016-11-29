68ff45fe3a82d9412a99b0a7b8afe786ec01c03d
Jhonatan Batista <jhonatan.silva@ccc.ufcg.edu.br>
2016-11-29 01:15:48 -0200

    Design

--- js/app.js
+++ js/app.js
@@ -1,17 +1,12 @@
-(function() {
-    var app = angular.module('toDoList', []);
-
-    app.component('toDoController', function() {
-        this.toDos = activities;
-    });
-
-    var activities = [{
-      name: "Prova de calculo",
-      description: "Prova doprimeiro estagio sobre limites",
-      completed: false
-    }, {
-      name: "Jogo de tenis"
-      description: "Segundo jogo 8 torneio da UFCG"
-      completed: false
-    }];
-})();
+var app = angular.module('toDoList', []);
+app.controller('ToDoController', function($scope) {
+  $scope.toDos = [{
+    name: "Prova de calculo",
+    description: "Prova doprimeiro estagio sobre limites",
+    completed: false
+  }, {
+    name: "Jogo de tenis",
+    description: "Segundo jogo 8 torneio da UFCG",
+    completed: false
+  }];
+});
